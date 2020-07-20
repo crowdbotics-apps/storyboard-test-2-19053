267d99b75f1af7a13ecc083caa002bdb6a0910f0
Crowdbotics <team@crowdbotics.com>
2020-07-20 22:33:51 +0000

    Update models

--- backend/storyboard_test_2_19053/wsgi.py
+++ backend/storyboard_test_2_19053/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'storyboard_test_2_19053.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "storyboard_test_2_19053.settings")
 
 application = get_wsgi_application()
